66a685549718a9b6da506123505e2b8c24289f05
Murali Krishna <mural.krish4u@gmail.com>
2024-10-17 01:20:51 +0530

    Updating color mode swithc color scheme.

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -4,7 +4,7 @@ const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
-      <Switch colorScheme='red' isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+      <Switch colorScheme='green' isChecked={colorMode === "dark"} onChange={toggleColorMode} />
       <Text whiteSpace='nowrap'> Dark Mode</Text>
     </HStack>
   );
